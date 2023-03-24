9ab307eeb1b4bc78fc337ea1b224dfef7e2e973d
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2023-03-24 14:06:22 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -176,7 +176,7 @@ elif view =='Show all data':
     st.write('Fight Stats')
     st.write(spark.sql("select * from fs limit 5"))    
 else:
-    st.line_chart(spark.sql("select to_date(DATE, 'MMMM d, yyyy') date,count(*) from fed group by 1 order by 1 asc"))
+    st.write(spark.sql("select to_date(DATE, 'MMMM d, yyyy') date,count(*) from fed group by 1 order by 1 asc"))
 
 
 
