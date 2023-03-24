4c460ba7c56b09f4bf9f4bc179c0301a08be4c7f
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2023-03-24 18:00:09 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -143,7 +143,8 @@ fight_results = fight_results.drop('OUTCOME',axis=1)
 if view =='Single Fighter Stats':
     fighter_list = list(fighters_df.select('FIGHTER').toPandas()['FIGHTER'])
     fighter_filter = st.selectbox('Pick a fighter',options=fighter_list)
-    fights = fight_results[fight_results['BOUT'].str.contains(fighter_filter,case=False)]
+    #fights = fight_results[fight_results['BOUT'].str.contains(fighter_filter,case=False)]
+    fights = list(spark.sql("select BOUT from fr_clean where BOUT like '%'{}'%'".format(fighter_filter)).toPandas())
     bouts = fight_stats[fight_stats['BOUT'].str.contains(fighter_filter, case=False)]
     opp_stats = fight_stats[(fight_stats['BOUT'].isin(bouts['BOUT'])) & (fight_stats['FIGHTER']!=fighter_filter)]
     fighter_stats = fight_stats[(fight_stats['BOUT'].isin(bouts['BOUT'])) & (fight_stats['FIGHTER']==fighter_filter)]
