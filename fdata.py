a5af4d1c110d3cb87b5cdea4142fd4d40c3d1d5a
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2023-03-24 13:52:58 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -131,6 +131,8 @@ fight_results = fight_results.drop('OUTCOME',axis=1)
 if view =='Single Fighter Stats':
     fighter_list = list(fighters_df.select('FIGHTER').toPandas()['FIGHTER'])
     fighter_filter = st.selectbox('Pick a fighter',options=fighter_list)
+    fightsz = fr.filter(FIGHTER==fighter_filter)
+    st.write(fightsz)
     fights = fight_results[fight_results['BOUT'].str.contains(fighter_filter,case=False)]
     bouts = fight_stats[fight_stats['BOUT'].str.contains(fighter_filter, case=False)]
     opp_stats = fight_stats[(fight_stats['BOUT'].isin(bouts['BOUT'])) & (fight_stats['FIGHTER']!=fighter_filter)]
