a7732db0725265237aed625db1ffdc59f7b7e4a0
ilya-peysakhov <ilya.peysakhov1@gmail.com>
2023-03-28 00:25:32 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -85,6 +85,7 @@ if view =='Single Fighter Stats':
     fighter_stats = duckdb.sql("SELECT * from fs_cleaned where BOUT in (select BOUT from fights) and FIGHTER ='{}' ".format(fighter_filter))
     opp_stats = duckdb.sql("SELECT * from fs_cleaned where BOUT in (select * from fights) and FIGHTER !='{}' ".format(fighter_filter))
     sig_abs = duckdb.sql("SELECT sum(sig_str_l::INTEGER) as s from opp_stats").df()
+    head_abs= duckdb.sql("SELECT sum(head_str_l::INTEGER) as s from opp_stats").df()
 
 
     if fighter_filter:
@@ -97,7 +98,7 @@ if view =='Single Fighter Stats':
                 st.write('Latest fight - '+str(last_fight['max_date'].values[0])+' -- '+str(last_fight['days_since'].values[0])+ ' ago')
         with col2:
             st.subheader(str(sig_abs['s'].values[0])+' Total Career Significant Strikes Absored')
-            st.subheader(str(head_absored['s'].values[0])+' Total Career Head Strikes Absored')
+            st.subheader(str(head_abs['s'].values[0])+' Total Career Head Strikes Absored')
         # with col3:
         #     st.subheader(str(sig_landed['s'].values[0])+' Total Career Significant Strikes Landed')
         #     st.subheader(str(head_landed['s'].values[0])+' Total Career Head Strikes Landed')
