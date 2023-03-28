8ee19810358021b17fc78e5f58487c451a57896f
ilya-peysakhov <ilya.peysakhov1@gmail.com>
2023-03-27 23:57:42 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -56,7 +56,7 @@ fr_cleaned = duckdb.sql("""SELECT trim(fr.EVENT) as EVENT,
                         from fr
                         left join fed on fed.URL = fr.URL""")
 fs = pl.read_csv("https://github.com/Greco1899/scrape_ufc_stats/raw/main/ufc_fight_stats.csv")
-fs_cleaned = duckdb.sql("""SELECT EVENT,BOUT,ROUND,FIGHTER,KD,
+fs_cleaned = duckdb.sql("""SELECT EVENT,BOUT,ROUND,trim(FIGHTER) as FIGHTER,KD,
                               split_part("SIG.STR.",' of ',1) sig_str_l,
                               split_part("SIG.STR.",' of ',2) sig_str_a,
                               split_part("TOTAL STR.",' of ',1) total_str_l,
@@ -69,7 +69,7 @@ fs_cleaned = duckdb.sql("""SELECT EVENT,BOUT,ROUND,FIGHTER,KD,
                               from fs """)
 frd = pl.read_csv("https://github.com/Greco1899/scrape_ufc_stats/raw/main/ufc_fighter_details.csv")
 ft = pl.read_csv("https://github.com/Greco1899/scrape_ufc_stats/raw/main/ufc_fighter_tott.csv")
-fighters= duckdb.sql("SELECT FIGHTER,HEIGHT,WEIGHT,REACH,STANCE,DOB,FIRST,LAST,NICKNAME,frd.URL from ft inner join frd on frd.URL = ft.URL")
+fighters= duckdb.sql("SELECT trim(FIGHTER) as FIGHTER,HEIGHT,WEIGHT,REACH,STANCE,DOB,FIRST,LAST,NICKNAME,frd.URL from ft inner join frd on frd.URL = ft.URL")
 ########################
                       
 
