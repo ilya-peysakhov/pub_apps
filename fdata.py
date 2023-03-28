80f21b4cbecfbba026d99f5f3102dba55812dcf4
ilya-peysakhov <ilya.peysakhov1@gmail.com>
2023-03-28 00:09:37 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -89,8 +89,8 @@ if view =='Single Fighter Stats':
 
 
     if fighter_filter:
-        st.write(fighter_stats.df())
-        st.write(opp_stats.df())
+        st.write(fights.df())
+   
         col1,col2,col3 = st.columns(3)
         with col1:
             st.write('Total UFC Fights - '+str(len(fights.df())))
