725c5488159d57d63bc073e0ba7fa67b0d002c15
ilya-peysakhov <ilya.peysakhov1@gmail.com>
2023-04-18 20:39:42 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -93,7 +93,7 @@ if view =='Fighter One Sheet':
         col1,col2,col3,col4,col5 = st.columns(5)
         with col1:
             st.subheader('Bio')
-            st.write(str(fighters.df()['HEIGHT']))
+            st.write(str(fighters.df().filter('FIGHTER'==fighter_filter)['HEIGHT']))
             if len(fights.df()) >0:
                 st.caption('Latest fight - '+str(last_fight['max_date'].values[0])+' - '+str(last_fight['days_since'].values[0])+ ' ago')
         with col2:
