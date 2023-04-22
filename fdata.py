25c8dae623f6a7fd29c96f78cf16924d7e7c0526
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2023-04-21 21:35:00 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -192,18 +192,18 @@ else:
 #         query = f"SELECT '{f}' AS FIGHTER,count(distinct BOUT||EVENT) as FIGHTS,count(*) as ROUNDS, SUM(head_str_l::INTEGER) AS HEAD_STRIKES_ABSORED,sum(KD::INTEGER) as KD, sum(TD_L::INT) as TD FROM fs_cleaned WHERE BOUT LIKE '%{f}%' AND fighter != '{f}' "
 #         result = duckdb.sql(query).df()
 #         str_results = pd.concat([str_results, result]) # Append the result to the DataFrame
-    query = f"""
-        SELECT '{f}' AS FIGHTER,
-               COUNT(DISTINCT CONCAT(BOUT, EVENT)) AS FIGHTS,
-               COUNT(*) AS ROUNDS,
-               SUM(CAST(head_str_l AS INT)) AS HEAD_STRIKES_ABSORED,
-               SUM(CAST(KD AS INT)) AS KD,
-               SUM(CAST(TD_L AS INT)) AS TD
-        FROM fs_cleaned
-        WHERE BOUT LIKE '%{f}%' AND fighter != '{f}'
-    """
-    result = pl.sql(query).to_pandas()
-    str_results = str_results.concat(result) 
+        query = f"""
+            SELECT '{f}' AS FIGHTER,
+                   COUNT(DISTINCT CONCAT(BOUT, EVENT)) AS FIGHTS,
+                   COUNT(*) AS ROUNDS,
+                   SUM(CAST(head_str_l AS INT)) AS HEAD_STRIKES_ABSORED,
+                   SUM(CAST(KD AS INT)) AS KD,
+                   SUM(CAST(TD_L AS INT)) AS TD
+            FROM fs_cleaned
+            WHERE BOUT LIKE '%{f}%' AND fighter != '{f}'
+        """
+        result = pl.sql(query).to_pandas()
+        str_results = str_results.concat(result) 
     st.write(str_results.set_index(str_results.columns[0]).sort_values(by='HEAD_STRIKES_ABSORED', ascending=False))   
 
 
