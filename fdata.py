c40f256c3e19ad3d33894eff63a251631f69e5be
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2023-04-22 17:08:10 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -187,7 +187,7 @@ elif view =='Show all dataset samples':
     st.write(duckdb.sql("SELECT * from fr_cleaned limit 5").df())
     st.write('Fight Stats')
     st.write(duckdb.sql("SELECT * from fs limit 5").df())
-    st.write("Data Check - last 50 events")
+    st.write("Data Check - Events without data")
     anomalies = duckdb.sql("select DATE,ed_c.EVENT, count(BOUT) as bouts_with_stats from ed_c left join fs on ed_c.EVENT =fs.EVENT group by 1,2 having bouts_with_stats=0 order by 1 desc").df()
     st.write(anomalies.set_index(anomalies.columns[0]))
 else:
