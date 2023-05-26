050888d22b97ad854a30dd52904f2f9113b5a6cc
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2023-05-26 12:13:55 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -51,7 +51,8 @@ fr_cleaned = duckdb.sql("""SELECT trim(fr.EVENT) as EVENT,
                         from fr
                         left join fed on fed.URL = fr.URL""")
 fs = pl.read_csv("https://github.com/Greco1899/scrape_ufc_stats/raw/main/ufc_fight_stats.csv")
-fs["FIGHTER"] = fs["FIGHTER"].str.replace("'", "")
+fs.to_pandas()
+fs['FIGHTER'] = fs['FIGHTER'].str.replace("'", "") 
 
 fs_cleaned = duckdb.sql("""SELECT fs.EVENT,replace(trim(BOUT),'  ',' ') as BOUT,ROUND, trim(FIGHTER) as FIGHTER,KD,
                               split_part("SIG.STR.",' of ',1) sig_str_l,
