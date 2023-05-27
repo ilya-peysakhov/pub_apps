4a15ef4e727518de11d04589681d82cd7dd27312
ilya-peysakhov <ilya.peysakhov1@gmail.com>
2023-05-27 13:00:42 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -251,7 +251,7 @@ else:
             SELECT FIGHTER2 fighter from fr_cleaned where date between current_date() -730 and current_date() group by 1)
             """).df().iloc[0,0]))
 
-        methods_over_time = duckdb.sql("SELECT METHOD, DATE_TRUNC(MONTH,DATE) MONTH, count(*) FIGHTS from fr_cleaned  group by 1,2 ")
+        methods_over_time = duckdb.sql("SELECT METHOD, DATE_TRUNC('month',DATE) MONTH, count(*) FIGHTS from fr_cleaned  group by 1,2 ")
         methods_over_time_chart = alt.Chart(methods_over_time).mark_area().encode(
         x='MONTH:O',
         y='FIGHTS:Q',
