78aa3164389673d3fb8e13f364c27ac37562917c
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2023-12-29 21:31:17 -0500

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -92,10 +92,7 @@ if view =='Fighter One Sheet':
         analysis_length = st.radio("Analysis Length (under development)",(analysis_lengths))
         
         if analysis_length==analysis_lengths[1]:
-            fr_cleaned = duckdb.sql("select * from fr_cleaned order by date").df()
-            fr_cleaned = fr_cleaned.head(3)
-            fr_cleaned = duckdb.sql("select * from fs_cleaned order by date").df()
-            fs_cleaned = fs_cleaned.head(3)
+            fr_cleaned = duckdb.sql("select * from fr_cleaned order by date limit 3").df()
     
     st.divider()
     
