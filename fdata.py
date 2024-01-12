4a2043ceb7581ea05bc95bb49adfae579efb5d92
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2024-01-12 11:47:21 -0500

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -85,19 +85,17 @@ fighters= duckdb.sql("SELECT trim(FIGHTER) as FIGHTER,HEIGHT,WEIGHT,REACH,STANCE
 if view =='Fighter One Sheet':
     st.text('Display all relevant fighter stats in just 1 click. Choose your fighter below to get started')
     fighter_list = duckdb.sql("SELECT FIGHTER from fighters  where length(DOB) >3 group by 1 order by 1").df()
-    f1, f2 , f3 = st.columns(3)
+    f1, f2  = st.columns(2)
     with f1:
         fighter_filter = st.selectbox('Pick a fighter',options=fighter_list)
     
-    
-    with st.expander('Custom Analysis Period'):
-        analysis_lengths = ['Career','Last X fights']
-        
-        analysis_length = st.radio("Analysis Length",(analysis_lengths))
-        
-        if analysis_length==analysis_lengths[1]:
-            al = st.number_input('Number of recent fights to analyze',step=1)
-            fr_cleaned = duckdb.sql(f"select * from fr_cleaned where FIGHTER1 = '{fighter_filter}' or FIGHTER2='{fighter_filter}' order by date desc limit {al}").df()
+    with f3:
+        with st.container(border=True):
+            analysis_lengths = ['Career','Last X fights']
+            analysis_length = st.radio("Analysis Length",(analysis_lengths))
+            if analysis_length==analysis_lengths[1]:
+                al = st.number_input('Number of recent fights to analyze',step=1)
+                fr_cleaned = duckdb.sql(f"select * from fr_cleaned where FIGHTER1 = '{fighter_filter}' or FIGHTER2='{fighter_filter}' order by date desc limit {al}").df()
     
     st.divider()
     
