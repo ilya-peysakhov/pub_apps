8e1ac6ebbf1a336788232277bfbbf6547d95f0ea
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2024-01-16 11:32:07 -0500

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -297,8 +297,7 @@ elif view =='Aggregate Table':
 elif view=='SQL Editor':
     st.write("Write custom sql on the data")
     st.write("Example below - Win % by age")
-    st.text_area(label='Query', value="""
-    select age,  sum(W) as wins, sum(L) as losses, sum(fights) as total_results, sum(W)/(sum(W)+sum(L)) as win_pct from 
+    st.code(label='Query', value="""select age,  sum(W) as wins, sum(L) as losses, sum(fights) as total_results, sum(W)/(sum(W)+sum(L)) as win_pct from 
      (
     select date_diff('year',strptime(dob, '%b %d, %Y'),date)  as age, sum (case when fighter1_outcome = 'W' then 1 end) W, sum (case when fighter1_outcome = 'L' then 1 end) as L, count(1) fights from fighters inner join fr_cleaned on fighter = fighter1 where (weightclass ilike '%welterweight%' or weightclass ilike '%middleweight%' or weightclass ilike '%heavyweight%')
     group by 1 
@@ -306,8 +305,7 @@ elif view=='SQL Editor':
     select date_diff('year',strptime(dob, '%b %d, %Y'),date)  as age, sum (case when fighter2_outcome = 'W' then 1 end) W, sum (case when fighter2_outcome = 'L' then 1 end) as L, count(1) fights from fighters inner join fr_cleaned on fighter = fighter2 where (weightclass ilike '%welterweight%' or weightclass ilike '%middleweight%' or weightclass ilike '%heavyweight%')
     group by 1 
      )
-    group by 1
-    
+    group by 1   
     """)
     col1,col2 = st.columns([3,10])
     with col1:
