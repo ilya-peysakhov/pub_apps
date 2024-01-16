04d4c20e210ebb5bd4b49ad2e87566b3ce5f82e3
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2024-01-16 13:11:11 -0500

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -330,11 +330,12 @@ elif view=='SQL Editor':
             
        
        
-with st.expander("Real UFC fans ONLY 🖱️",expanded=False):
-   audio_file = open('song.mp3', 'rb')
-   audio_bytes = audio_file.read()
-   st.audio(audio_bytes, format='audio/ogg')        
+     
 
 st.code('Built by Ilya')
 st.code('This application uses data from Greco1899''s scraper of UFC Fight Stats - "https://github.com/Greco1899/scrape_ufc_stats"')
 st.code('Recent changes - SQL Editor, data retrieval cached via function' )
+with st.expander("Real UFC fans ONLY 🖱️",expanded=False):
+   audio_file = open('song.mp3', 'rb')
+   audio_bytes = audio_file.read()
+   st.audio(audio_bytes, format='audio/ogg')   
