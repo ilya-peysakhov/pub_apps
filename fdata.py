cd95421515fe16b9ee06f39ab0f80a454c36f4b6
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2024-03-12 22:58:53 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -358,11 +358,14 @@ elif view =='Aggregate Table':
       query = f"SELECT '{fighter}' AS FIGHTER, SUM(head_str_l::INTEGER) AS HEAD_STRIKES_ABS, SUM(head_str_a::INTEGER) AS HEAD_STRIKES_AT, SUM(sig_str_l::INTEGER) AS SIG_STRIKES_ABS, SUM(leg_str_l::INTEGER) as LEG_STRIKES_ABSORBED, sum(KD::INTEGER) as KD_ABSORED, sum(TD_L::INT) as TD_GIVEN_UP FROM fs_cleaned WHERE BOUT LIKE '%{fighter}%' AND fighter != '{fighter}'"
       return duckdb.sql(query).df()
     
-    str_results = pd.concat(fighters['FIGHTER'].apply(query_fighter_data).tolist(), ignore_index=True)
+    @st.cache_data(ttl='6d')
+    def oppStats():
+      str_results = pd.concat(fighters['FIGHTER'].apply(query_fighter_data).tolist(), ignore_index=True)
+      return str_results
     # dfs_list = [query_fighter_data(fighter) for fighter in fighters['FIGHTER']]
     # str_results = pd.concat(dfs_list, ignore_index=True)
 
-  
+    str_results = oppStats()
     combined_stats = duckdb.sql("SELECT a.*, ROUND(SIG_STRIKES_LANDED/SIG_STRIKES_ABS,1) as SIG_STR_DIFF, ROUND((1-HEAD_STRIKES_ABS/HEAD_STRIKES_AT),2) as HEAD_MOVEMENT, b.* EXCLUDE (FIGHTER) from all_time_offense as a left join str_results as b on a.FIGHTER=b.FIGHTER").df()
     st.dataframe(combined_stats.sort_values(by='FIGHTS', ascending=False),hide_index=True)   
 elif view=='SQL Editor':
