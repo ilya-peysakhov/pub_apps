72809a34a22be6a5576b5e41f14dc1a8d090fb78
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2024-03-17 20:24:10 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -106,8 +106,8 @@ if view=='Welcome':
               
   st.caption('Please note that this a free, hosted application with data gathered by a 3rd party and not everything will be perfectly working at all times. However if you are a hardcore MMA fan, please use as you like. If you have questions or suggestions, a suggestion box will be introduced soon.') 
 
-   if "inputs" not in st.session_state:
-      st.session_state.inputs = []
+ if "inputs" not in st.session_state:
+    st.session_state.inputs = []
 
   # Function to add text area input to the list
   def add_input():
