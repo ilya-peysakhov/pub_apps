a59a6f586fda2fa752aa2701638efd4cdb6063c3
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2024-03-28 15:05:57 -0400

    Update fdata.py

--- fdata.py
+++ fdata.py
@@ -358,16 +358,26 @@ elif view =='Interesting Stats':
 elif view =='Aggregate Table':      
     min_fights = st.number_input('Minimum Fights',step=1,value=20)
     st.write(f"Minimum {min_fights} fights, historical rankings for total career offensive and defensive stats")
-    
-    fighters = duckdb.sql(f"SELECT fighter FROM fs_cleaned GROUP BY 1 having count(distinct BOUT||EVENT) >={min_fights} ").df()
+    @st.cache_data(ttl='6d')
+    def getFighters(min_fights):
+        fighters = duckdb.sql(f"SELECT fighter FROM fs_cleaned GROUP BY 1 having count(distinct BOUT||EVENT) >={min_fights}").df()
+        return fighters
+    fighters = getFighters(min_fights)
     str_results = pd.DataFrame()
-    all_time_offense = duckdb.sql(f"SELECT FIGHTER, COUNT(DISTINCT BOUT||EVENT) as FIGHTS, COUNT(*) AS ROUNDS,  ROUND(ROUNDS/CAST(FIGHTS as REAL),1) as ROUNDS_PER_FIGHT ,SUM(head_str_l::INTEGER) AS HEAD_STRIKES_LANDED, SUM(leg_str_l::INTEGER) as LEG_STRIKES_LANDED,sum(sig_str_l::INTEGER) as SIG_STRIKES_LANDED,sum(KD::INTEGER) as KD_LANDED, sum(TD_L::INT) as TD_LANDED from fs_cleaned group by 1 having FIGHTS>={min_fights}")
     
+    @st.cache_data(ttl='6d')
+    def getOffense():
+        all_time_offense = duckdb.sql(f"SELECT FIGHTER, COUNT(DISTINCT BOUT||EVENT) as FIGHTS, COUNT(*) AS ROUNDS,  ROUND(ROUNDS/CAST(FIGHTS as REAL),1) as ROUNDS_PER_FIGHT ,SUM(head_str_l::INTEGER) AS HEAD_STRIKES_LANDED, SUM(leg_str_l::INTEGER) as LEG_STRIKES_LANDED,sum(sig_str_l::INTEGER) as SIG_STRIKES_LANDED,sum(KD::INTEGER) as KD_LANDED, sum(TD_L::INT) as TD_LANDED from fs_cleaned group by 1 having FIGHTS>={min_fights}")
+        return all_time_offense
+    
+    all_time_offense = getOffense()
+    
+    @st.cache_data(ttl='6d')
     def query_fighter_data(fighter):
       query = f"SELECT '{fighter}' AS FIGHTER, SUM(head_str_l::INTEGER) AS HEAD_STRIKES_ABS, SUM(head_str_a::INTEGER) AS HEAD_STRIKES_AT, SUM(sig_str_l::INTEGER) AS SIG_STRIKES_ABS, SUM(leg_str_l::INTEGER) as LEG_STRIKES_ABSORBED, sum(KD::INTEGER) as KD_ABSORED, sum(TD_L::INT) as TD_GIVEN_UP FROM fs_cleaned WHERE BOUT LIKE '%{fighter}%' AND fighter != '{fighter}'"
       return duckdb.sql(query).df()
     
-
+    @st.cache_data(ttl='6d')
     def oppStats():
       dfs_list = fighters['FIGHTER'].apply(query_fighter_data).tolist()
       str_results = pd.concat(dfs_list, ignore_index=True)
