b3aa6eb5b570e44a144c1b3df79b319e537a30c7
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2026-05-17 00:17:28 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_icon="👊", page_title="UFC Stats Explorer v1.0", layout="wide",initial_sidebar_state='collapsed')
+
+st.html("assets/style.css")
+
+pg = st.navigation([st.Page("pages/fdata.py")])
+pg.run()
