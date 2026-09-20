e9cf96af1f38ed1b99efea1d23b2ca3caba9e6f9
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2026-09-19 23:15:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-
+from pathlib import Path
 st.set_page_config(page_icon="👊", page_title="UFC Stats Explorer v1.0", layout="wide",initial_sidebar_state='collapsed')
 
-st.html("assets/style.css")
+st.html(Path("assets/style.css"))
 
 pg = st.navigation(
   [st.Page("pages/fdata.py")]
